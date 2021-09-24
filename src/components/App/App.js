f01510c4a07433f47ccfe6044f77ee28b7e72413
Eric Li <ericli@knights.ucf.edu>
2021-09-24 14:43:38 -0600

    Import and implement StartLoader and Search components into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,40 @@
-import logo from '../../images/logo.svg';
+
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import { fetchStockList } from '../../apiCalls';
+import StartLoader from '../StartLoader/StartLoader';
+import Search from '../Search/Search';
 
 const App = () => {
   const [stockList, setStockList] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
   const [stockListError, setStockListError] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
+    setIsLoading(true)
     updateStockList()
-    setIsLoading(false)
+    setTimeout(() => setIsLoading(false), 3000)
   }, [])
 
   const updateStockList = () => {
-    setIsLoading(true)
-    fetchStockList()
-      .then(data => setStockList(data))
+      fetchStockList()
+      .then(data => setStockList(data.slice(0, 10000)))
       .catch(error => setStockListError(error.message))
-    
   }
 
   return (
     <div className="App">
       <header className="App-header">
-
+        { isLoading ? <StartLoader /> : <Search />}
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+// { isLoading === 1 && <div class='loader'>
+// <h1>HELLO</h1>
+// <img className="capita-logo" src={capitaLogo}></img></div>
+// }
