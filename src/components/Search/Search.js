0e292d23a8aeeb1179135690e8c677219ab30323
Eric Li <ericli@knights.ucf.edu>
2021-09-24 14:45:31 -0600

    Build initial Search component

---
+++ src/components/Search/Search.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './Search.scss';
+
+const Search = () => {
+  const [query, setQuery] = useState('')
+
+  return (
+    <form className="search-stocks">
+      <input className="search-input" type="text"></input>
+    </form>
+  )
+}
+
+export default Search;
