78f538a01fd080a82e8187ec6df6147abb0b74e7
Eric Li <ericli@knights.ucf.edu>
2021-09-24 23:38:10 -0600

    Finish basic Search component, add onChange and refactor link path

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,13 +2,24 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Search.scss';
 
-const Search = ({stocks}) => {
+const Search = ({ stocks }) => {
   const [query, setQuery] = useState('')
+
+  const handleFormChange = (event) => {
+    event.preventDefault()
+    setQuery(event.target.value)
+  }
 
   return (
     <form className="search-stocks">
-      <input className="search-input" type="text" value={query} placeholder="Search by stock or ticker name (ex. Apple or AAPL)"></input>
-      <Link to={`/${query}`}>
+      <input 
+        className="search-input" 
+        type="text"
+        placeholder="Search by stock or ticker name (ex. Apple or AAPL)"
+        onChange={event => handleFormChange(event)}
+        value={query} required>
+      </input>
+      <Link to={`/stock/${query.toUpperCase()}`}>
         <button className="search-submit">GO</button>
       </Link>
     </form>
