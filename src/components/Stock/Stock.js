e24c70a5decc94307b44e3affeb8821c4acb0106
Eric Li <ericli@knights.ucf.edu>
2021-09-24 23:41:42 -0600

    Build initial Stock component

---
+++ src/components/Stock/Stock.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+const Stock = ({ updateStockDetail, stockDetail, query }) => {
+  const [stockData, setStockData] = useState([])
+
+  useEffect(() => {
+    updateStockDetail(query)
+  }, [])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Stock;
