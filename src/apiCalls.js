e418f420c42511bb31e47c748fe66c61b66963f3
Eric Li <ericli@knights.ucf.edu>
2021-09-24 23:35:48 -0600

    Change fetchStockList to fetchFortuneList

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
-export const fetchStockList = () => {
-  const url = 'https://financialmodelingprep.com/api/v3/stock/list?apikey=1c2f080351d9a4329c6b22ef21dc46dc'
+export const fetchFortuneList = () => {
+  const url = 'https://financialmodelingprep.com/api/v3/sp500_constituent?apikey=1c2f080351d9a4329c6b22ef21dc46dc'
   return fetch(url)
     .then(response => {
       if (response.ok) {
@@ -20,7 +20,7 @@ export const fetchStockDetail = (company) => {
         return response.json()
       } else if (response.status === 404) {
         throw new Error('404 Error - Stock or ticker name not found. Please go back and try your search again.')
-      } else if {response.status === 500} {
+      } else if (response.status === 500) {
         throw new Error('500 Error - Encountered server error. Please try again.')
       } else {
         throw new Error('Other Error - Something went wrong.')
