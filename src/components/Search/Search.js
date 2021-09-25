aec3cdc232c08ff7f1ca7b16486602eabba93b25
Eric Li <ericli@knights.ucf.edu>
2021-09-24 19:23:12 -0600

    Add stocks prop to Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './Search.scss';
 
-const Search = () => {
+const Search = ({stocks}) => {
   const [query, setQuery] = useState('')
 
   return (
     <form className="search-stocks">
       <input className="search-input" type="text"></input>
+      
     </form>
   )
 }
