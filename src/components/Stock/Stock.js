90c6776250123b35d70f7a68454bccd384170d6d
Eric Li <ericli@knights.ucf.edu>
2021-09-25 23:32:32 -0600

    Add async function startMountStock, add three dots loader

--- src/components/Stock/Stock.js
+++ src/components/Stock/Stock.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import Loader from 'react-loader-spinner';
 
 const Stock = ({ updateStockDetail, stockDetail, query }) => {
   const [stockData, setStockData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-    updateStockDetail(query)
+    startMountStock()
   }, [])
+
+  const startMountStock = async () => {
+    setIsLoading(true)
+    await updateStockDetail(query)
+    setTimeout(() => setIsLoading(false), 2500)
+  }
 
   return (
     <div>
-
+      { isLoading ? <Loader className="three-dots" type="ThreeDots" color="#ffffff" height="50" /> :
+      <div></div>}
     </div>
   )
 }
