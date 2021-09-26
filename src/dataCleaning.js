dd1bf222b666ebde90f957cae58e8b92e076cbc2
Eric Li <ericli@knights.ucf.edu>
2021-09-26 17:08:08 -0600

    Add function dollarize number, remove thousands

--- src/dataCleaning.js
+++ src/dataCleaning.js
@@ -4,15 +4,18 @@ const thousands = require('thousands');
 export const cleanStockDetailData = (stockDetailData) => {
   return stockDetailData.map(history => {
     const date = history.date.split(' ')[0]
-    const time = history.date.split(' ')[1]
     return {
       date: dayjs(date).format('MMM DD, YYYY'),
       time: dayjs(history.date).format('h:mm A'),
-      open: `$${thousands(history.open.toFixed(2))}`,
-      close: `$${thousands(history.close.toFixed(2))}`,
-      low: `$${thousands(history.low.toFixed(2))}`,
-      high: `$${thousands(history.high.toFixed(2))}`,
-      volume: thousands(history.volume)
+      open: history.open,
+      close: history.close,
+      low: history.low,
+      high: history.high,
+      volume: history.volume
     }
   })
+}
+
+export const dollarizeNumber = (number) => {
+  return `$${number.toFixed(2)}`
 }
