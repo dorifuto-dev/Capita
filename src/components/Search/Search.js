754bf05aa87959f0dc95d12483b320f49c0add05
Eric Li <ericli@knights.ucf.edu>
2021-09-27 00:11:39 -0600

    Add second field for exchange for search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,26 +4,38 @@ import './Search.scss';
 
 const Search = ({ stocks }) => {
   const [query, setQuery] = useState('')
+  const [exchange, setExchange] = useState('')
 
   const handleFormChange = (event) => {
     event.preventDefault()
-    setQuery(event.target.value)
+    if (event.target.id === "exchangeSelect") {
+      setExchange(event.target.value)
+    } else {
+      setQuery(event.target.value)
+    }
   }
 
   return (
-    <form className="search-stocks">
+    <form className="search-stocks" onChange={(event) => handleFormChange(event)}>
       <input 
         className="search-input" 
         type="text"
-        placeholder="Search by ticker name (ex. AAPL for Apple)"
+        placeholder="Search by company or ticker name (ex. AAPL or Apple)"
         onChange={event => handleFormChange(event)}
         value={query} required>
       </input>
-      <Link to={`/stock/${query.toUpperCase()}`}>
-        <button className="search-submit">GO</button>
-      </Link>
+      <div>
+      <select name="exchange" id="exchangeSelect" className="search-exchange">
+        <option value='' disabled={true}>- Select a Stock Exchange -</option>
+        <option value="NYSE">NYSE</option>
+        <option value="NASDAQ">NASDAQ</option>
+      </select>
+      <button className="search-submit">GO</button>
+      </div>
     </form>
   )
 }
 
 export default Search;
+
+{/* <Link to={`/stock/${query.toUpperCase()}`}> */}
