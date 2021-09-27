7e308cb7852feabf0edfd8a70c357432cd8dd14c
Eric Li <ericli@knights.ucf.edu>
2021-09-27 03:35:13 -0600

    Add getSearchResultLinks function, add conditional rendering

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -22,6 +22,7 @@ const Search = ({ stocks }) => {
     event.preventDefault()
     if (query && exchange) {
       await fetchSearchSuggestions(query, exchange)
+        .then(data => createSearchResultLinks(data))
         .then(data => setSearchResults(data))
         .catch(error => setSearchError(error.message))
     } else {
@@ -30,31 +31,40 @@ const Search = ({ stocks }) => {
     }
   }
 
+  const createSearchResultLinks = (data) => {
+    return data.map((result, index) => {
+      return (<Link to={`/stock/${result.symbol}`}><button className="stock-link">{result.name}</button></Link>)
+    })
+  }
+
   return (
-    <form className="search-stocks">
-      <input 
-        className="search-input" 
-        type="text"
-        placeholder="Search by company or ticker name (ex. AAPL or Apple)"
-        onChange={event => handleFormChange(event)}
-        value={query} required>
-      </input>
-      <div>
-      <select 
-        name="exchange" 
-        id="exchangeSelect" 
-        className="search-exchange"
-        value={exchange}
-        onChange={event => handleFormChange(event)}
-      >
-        <option value='' disabled={true}>- Select a Stock Exchange -</option>
-        <option value="NYSE">NYSE</option>
-        <option value="NASDAQ">NASDAQ</option>
-      </select>
-      <button className="search-submit" onClick={(event) => getSearchResults(event, query, exchange)}>GO</button>
-      { searchError && <p className="incomplete-form-error">{searchError}</p>}
-      </div>
-    </form>
+    <section className="search-container">
+      <form className="search-stocks">
+        <input 
+          className="search-input" 
+          type="text"
+          placeholder="Search by ticker name (ex. AAPL for Apple)"
+          onChange={event => handleFormChange(event)}
+          value={query} required>
+        </input>
+        <div className="exchange-submit-group">
+        <select 
+          name="exchange" 
+          id="exchangeSelect" 
+          className="search-exchange"
+          value={exchange}
+          onChange={event => handleFormChange(event)}
+        >
+          <option value='' disabled={true}>- Select a Stock Exchange -</option>
+          <option value="NYSE">NYSE</option>
+          <option value="NASDAQ">NASDAQ</option>
+        </select>
+        <button className="search-submit" onClick={(event) => getSearchResults(event, query, exchange)}>GO</button>
+        { searchError && <p className="incomplete-form-error">{searchError}</p>}
+        </div>
+      </form>
+      { searchResults && <section className="search-results">{searchResults}</section>}
+    </section>
   )
 }
 
