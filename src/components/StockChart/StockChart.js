6bbf7f9c79182113d5ce01f0469cf68d107b4eeb
Eric Li <ericli@knights.ucf.edu>
2021-09-26 22:36:39 -0600

    Build initial StockChart component, edit chart options

--- src/components/StockChart/StockChart.js
+++ src/components/StockChart/StockChart.js
@@ -1,23 +1,68 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { Chart } from 'chart.js';
+import { Chart, registerables } from 'chart.js';
 import './StockChart.css';
 
-const chartConfig = {
-  type: 'line',
-  data: {
-    // Bring in Data from Stock as prop
-  }
-}
+Chart.defaults.scale.display = false;
+Chart.defaults.maintainAspectRatio = false;
+Chart.defaults.responsive = true;
 
-const StockChart = () => {
-  const chartRef = useRef(null)
+console.log(Chart.defaults)
+
+Chart.register(...registerables)
+
+const StockChart = ({stockDetail}) => {
+  const chartContainer = useRef(null)
   const [chartInstance, setChartInstance] = useState(null)
 
-  useEffect(() => {
-    if (chartRef && chartRef.current) {
-      const newChartInstance = new Chart(chartRef.current, setChartInstance(newChartInstance))
+  const chartConfig = {
+    type: 'line',
+    data: {
+      labels: stockDetail.map(detail => detail.dateTime).reverse(),
+      datasets: [
+        {
+          label: "Stock Price",
+          data: stockDetail.map(detail => detail.close.toFixed(2)).reverse(),
+          borderColor: "#ffffff",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      layout: {
+        padding: {
+          top: 5,
+          left: 15,
+          right: 15,
+          bottom: 15
+        }
+      },
+      elements: {
+        point: {
+          hitRadius: 10,
+          hoverRadius: 10,
+          radius: 0
+        }
+      }
     }
-  }, [])
+  }
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chart(chartContainer.current, chartConfig)
+      setChartInstance(newChartInstance)
+    }
+  }, [chartContainer])
+
+  return (
+    <div className="chart-container">
+      <canvas className="stock-chart" ref={chartContainer} />
+    </div>
+  )
 }
 
 export default StockChart;
