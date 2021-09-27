dabf37bbe083e092bedfd475f669f58c992b97f8
Eric Li <ericli@knights.ucf.edu>
2021-09-26 22:35:51 -0600

    Import thousands, refactor conditional, add details

--- src/components/Stock/Stock.js
+++ src/components/Stock/Stock.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { dollarizeNumber } from '../../dataCleaning';
 import StockChart from '../StockChart/StockChart';
 import Loader from 'react-loader-spinner';
 import './Stock.scss';
+const thousands = require('thousands');
 
 const Stock = ({ updateStockDetail, stockDetail, query }) => {
   const [stockData, setStockData] = useState([])
@@ -14,17 +16,23 @@ const Stock = ({ updateStockDetail, stockDetail, query }) => {
   const mountStock = async () => {
     setIsLoading(true)
     await updateStockDetail(query)
+    await setStockData(stockDetail)
     setTimeout(() => setIsLoading(false), 2500)
   }
 
   return (
     <div>
-      { isLoading ? <Loader className="three-dots" type="ThreeDots" color="#ffffff" height="50" /> :
-      <div>
-        <StockChart 
-          stockDetail={stockDetail}
-        />
-      </div>}
+      { (isLoading || !stockDetail) ? <Loader className="three-dots" type="ThreeDots" color="#ffffff" height="50" /> :
+        <div>
+          <StockChart 
+            stockDetail={stockDetail}
+          />
+          <h1 className="stock-title">{query}</h1>
+          <p className="last-updated">{`Last updated on ${stockDetail[0].dateTime}`}</p>
+          <p className="title">{`Latest Price: ${dollarizeNumber(stockDetail[0].close)}`}</p>
+          
+        </div>
+      }
     </div>
   )
 }
@@ -32,3 +40,4 @@ const Stock = ({ updateStockDetail, stockDetail, query }) => {
 export default Stock;
 
 //MAY CHANGE NAME TO DETAIL, CHART, etc.
+
