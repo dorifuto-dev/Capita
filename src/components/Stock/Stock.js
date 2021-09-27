2f1cca11749434dd2f67e08bfc72664d86a1f661
Eric Li <ericli@knights.ucf.edu>
2021-09-27 16:53:26 -0600

    Remove extra code, fix spacing

--- src/components/Stock/Stock.js
+++ src/components/Stock/Stock.js
@@ -3,10 +3,8 @@ import { dollarizeNumber } from '../../dataCleaning';
 import StockChart from '../StockChart/StockChart';
 import Loader from 'react-loader-spinner';
 import './Stock.scss';
-const thousands = require('thousands');
 
 const Stock = ({ updateSavedStocks, updateStockDetail, stockDetail, ticker, company }) => {
-  const [stockData, setStockData] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
@@ -16,31 +14,28 @@ const Stock = ({ updateSavedStocks, updateStockDetail, stockDetail, ticker, comp
   const mountStock = async () => {
     setIsLoading(true)
     await updateStockDetail(ticker)
-    await setStockData(stockDetail)
     setTimeout(() => setIsLoading(false), 2500)
   }
 
   return (
     <div>
       { (isLoading || !stockDetail) ? <Loader className="three-dots" type="ThreeDots" color="#ffffff" height="50" /> :
-        <div>
-         
+        <article className="stock-component">
           <section className="stock-info">
             <h1 className="ticker-title">{ticker}</h1>
             <h1 className="company-title">{company}</h1>
             <p className="last-updated">{`Last updated on ${stockDetail[0].dateTime}`}</p>
             <p className="latest-price">{`Latest Price: ${dollarizeNumber(stockDetail[0].close)}`}</p>
+            <button className="favorite-stock" onClick={(event) => updateSavedStocks(event, {name: company, id: Date.now(), symbol: ticker})}>Favorite Stock</button>
           </section>
           <StockChart 
             stockDetail={stockDetail}
           />
-        </div>
+        </article>
       }
     </div>
   )
 }
 
 export default Stock;
-
-//MAY CHANGE NAME TO DETAIL, CHART, etc.
 
