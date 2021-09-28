252c0af60a625211d5758de9aeba16a99e0fe904
Eric Li <ericli@knights.ucf.edu>
2021-09-27 19:20:16 -0600

    Remove extra fetching function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,17 +1,3 @@
-export const fetchFortuneList = () => {
-  const url = 'https://financialmodelingprep.com/api/v3/sp500_constituent?apikey=1c2f080351d9a4329c6b22ef21dc46dc'
-  return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else if (response.status === 500) {
-        throw new Error('500 Error - Encountered server error. Please try again.')
-      } else {
-        throw new Error('Other Error - Something went wrong.')
-      }
-    })
-}
-
 export const fetchStockDetail = (company) => {
   const url = `https://financialmodelingprep.com/api/v3/historical-chart/15min/${company}?apikey=1c2f080351d9a4329c6b22ef21dc46dc`
   return fetch(url)
